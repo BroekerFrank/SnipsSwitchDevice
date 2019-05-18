6de88400ea4d241b50071528ef580e67febff5b7
BroekerFrank <50699456+BroekerFrank@users.noreply.github.com>
2019-05-18 18:13:13 +0200

    Create action-switchDevice.py

---
+++ action-switchDevice.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import paho.mqtt.client as mqtt
+import json
+
+# MQTT client to connect to the bus
+mqtt_client = mqtt.Client()
+
+
+def on_connect(client, userdata, flags, rc):
+    client.subscribe("hermes/intent/#")
+
+
+def on_message(client, userdata, msg):
+    data = json.loads(msg.payload.decode('utf8'))
+    intentname = data['intent']['intentName']
+    if intentname == "BroekerFrank:switchOnIntent":
+        slots = parse_slots(data)
+		text = "Das Gerät " + slots+ " wurde eingeschaltet."
+        mqtt_client.publish('hermes/dialogueManager/endSession', json.dumps({'text': text, "sessionId": session_id}))
+
+    if intentname == "BroekerFrank:switchOffIntent":
+        slots = parse_slots(data)
+		text = "Das Gerät " + slots+ " wurde ausgeschaltet."
+        mqtt_client.publish('hermes/dialogueManager/endSession', json.dumps({'text': text, "sessionId": session_id}))	
+
+def parse_slots(data):
+    # We extract the slots as a dict
+    return {slot['slotName']: slot['value']['value'] for slot in data['slots']}
+
+
+if __name__ == "__main__":
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+    mqtt_client.connect("localhost", 1883)
+mqtt_client.loop_forever()
