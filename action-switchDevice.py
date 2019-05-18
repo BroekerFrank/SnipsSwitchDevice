395e6d35dd94f6f5e7581fe305edf40672740e68
BroekerFrank <50699456+BroekerFrank@users.noreply.github.com>
2019-05-18 18:48:58 +0200

    Update action-switchDevice.py

--- action-switchDevice.py
+++ action-switchDevice.py
@@ -17,12 +17,12 @@ def on_message(client, userdata, msg):
     intentname = data['intent']['intentName']
     if intentname == "BroekerFrank:switchOnIntent":
         slots = parse_slots(data)
-		text = "Das Gerät " + slots+ " wurde eingeschaltet."
+	text = "Das Gerät " + slots+ " wurde eingeschaltet."
         mqtt_client.publish('hermes/dialogueManager/endSession', json.dumps({'text': text, "sessionId": session_id}))
 
     if intentname == "BroekerFrank:switchOffIntent":
         slots = parse_slots(data)
-		text = "Das Gerät " + slots+ " wurde ausgeschaltet."
+	text = "Das Gerät " + slots+ " wurde ausgeschaltet."
         mqtt_client.publish('hermes/dialogueManager/endSession', json.dumps({'text': text, "sessionId": session_id}))	
 
 def parse_slots(data):
